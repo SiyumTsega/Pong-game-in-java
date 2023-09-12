f498b96b0131efc734cbaf072238e0246514b0ee
Siyum Tsega <siyum3287@gmai.com>
2023-09-12 12:03:20 +0300

    Initial Commit.

---
+++ src/pong/PongGame.java
@@ -0,0 +1,16 @@
+package pong;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+public class PongGame {
+
+	PongGame(){
+		
+	}
+	public static void main(String[] args) {
+
+		GameFrame frame = new GameFrame();
+	}
+
+}
